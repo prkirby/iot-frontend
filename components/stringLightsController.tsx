408015ee9061ea9e7bb13314a639d26df0788622
Paul Kirby <paulrdkirby@gmail.com>
2023-09-22 00:40:12 -0500

    Move Stringlights controller into component

---
+++ components/stringLightsController.tsx
@@ -0,0 +1,300 @@
+import { useState, useRef } from 'react'
+import type { MqttClient } from 'mqtt'
+import useMqtt from '../lib/useMqtt'
+import { handlerPayload } from '../lib/useMqtt'
+
+import {
+  Box,
+  // Button,
+  TextField,
+  Stack,
+  Container,
+  Typography,
+  Slider,
+  Switch,
+} from '@mui/material'
+
+interface StringLightsControllerProps {
+  topicPrefix: string
+  name: string
+}
+
+export default function StringLightsController({
+  topicPrefix,
+  name,
+}: StringLightsControllerProps) {
+  const [ledEnabled, setLedEnabled] = useState(false)
+  const [ledDuty, setLedDuty] = useState(0)
+  const [animEnabled, setAnimEnabled] = useState(false)
+  const [minSinDuty, setMinSinDuty] = useState(0)
+  const [maxSinDuty, setMaxSinDuty] = useState(50)
+  const [animTime, setAnimTime] = useState(4000)
+
+  const incommingMessageHandlers = useRef([
+    {
+      topic: topicPrefix + '/ledState',
+      handler: ({ payload }: handlerPayload) => {
+        setLedEnabled(!!payload)
+      },
+    },
+    {
+      topic: topicPrefix + '/ledDutyState',
+      handler: ({ payload }: handlerPayload) => {
+        setLedDuty(payload)
+      },
+    },
+    {
+      topic: topicPrefix + '/ledAnimState',
+      handler: ({ payload }: handlerPayload) => {
+        setAnimEnabled(!!payload)
+      },
+    },
+    {
+      topic: topicPrefix + '/minSinDutyState',
+      handler: ({ payload }: handlerPayload) => {
+        setMinSinDuty(payload)
+      },
+    },
+    {
+      topic: topicPrefix + '/maxSinDutyState',
+      handler: ({ payload }: handlerPayload) => {
+        setMaxSinDuty(payload)
+      },
+    },
+    {
+      topic: topicPrefix + '/animTimeState',
+      handler: ({ payload }: handlerPayload) => {
+        setAnimTime(payload)
+      },
+    },
+  ])
+
+  const mqttClientRef = useRef<MqttClient | null>(null)
+  const setMqttClient = (client: MqttClient) => {
+    mqttClientRef.current = client
+  }
+  useMqtt({
+    uri: process.env.NEXT_PUBLIC_MQTT_URI,
+    options: {
+      // username: process.env.NEXT_PUBLIC_MQTT_USERNAME,
+      // password: process.env.NEXT_PUBLIC_MQTT_PASSWORD,
+      clientId: process.env.NEXT_PUBLIC_MQTT_CLIENTID + '_' + topicPrefix,
+    },
+    topicHandlers: incommingMessageHandlers.current,
+    onConnectedHandler: (client) => setMqttClient(client),
+  })
+
+  const mainLedOn = (client: any) => {
+    if (!client) {
+      console.log('(ledEnable) Cannot publish, mqttClient: ', client)
+      return
+    }
+
+    client.publish(topicPrefix + '/ledEnable', 'true')
+  }
+
+  const mainLedOff = (client: any) => {
+    if (!client) {
+      console.log('(ledDisable) Cannot publish, mqttClient: ', client)
+      return
+    }
+
+    client.publish(topicPrefix + '/ledDisable', 'true')
+  }
+
+  const ledAnimOn = (client: any) => {
+    if (!client) {
+      console.log('(ledAnimEnable) Cannot publish, mqttClient: ', client)
+      return
+    }
+
+    client.publish(topicPrefix + '/ledAnimEnable', 'true')
+  }
+
+  const ledAnimOff = (client: any) => {
+    if (!client) {
+      console.log('(ledAnimOff) Cannot publish, mqttClient: ', client)
+      return
+    }
+
+    client.publish(topicPrefix + '/ledAnimDisable', 'true')
+  }
+
+  const sendLedDuty = (client: any, ledDuty: number) => {
+    if (!client) {
+      console.log('(setLedDuty) Cannot publish, mqttClient: ', client)
+      return
+    }
+
+    client.publish(topicPrefix + '/setLedDuty', ledDuty.toString(10))
+  }
+
+  const sendMinSinDuty = (client: any, minSinDuty: number) => {
+    if (!client) {
+      console.log('(setMinSinDuty) Cannot publish, mqttClient: ', client)
+      return
+    }
+
+    client.publish(topicPrefix + '/setMinSinDuty', minSinDuty.toString(10))
+  }
+
+  const sendMaxSinDuty = (client: any, maxSinDuty: number) => {
+    if (!client) {
+      console.log('(setMaxSinDuty) Cannot publish, mqttClient: ', client)
+      return
+    }
+
+    client.publish(topicPrefix + '/setMaxSinDuty', maxSinDuty.toString(10))
+  }
+
+  const sendAnimTime = (client: any, animTime: number) => {
+    if (!client) {
+      console.log('(setAnimTime) Cannot publish, mqttClient: ', client)
+      return
+    }
+
+    client.publish(topicPrefix + '/setAnimTime', animTime.toString(10))
+  }
+
+  return (
+    <Container>
+      <Box my={6}>
+        <Typography variant="h3">{name}</Typography>
+        <Box my={4}>
+          <Stack my={2} direction="row" spacing={2} alignItems="left">
+            <Typography variant="h4">LED Enabled:</Typography>
+            <Switch
+              checked={ledEnabled}
+              size="medium"
+              onChange={(e) => {
+                const ledEnabled = e.target.checked
+                if (ledEnabled) {
+                  mainLedOn(mqttClientRef.current)
+                } else {
+                  mainLedOff(mqttClientRef.current)
+                }
+                setLedEnabled(ledEnabled)
+              }}
+            />
+          </Stack>
+          <Stack my={2} direction="row" spacing={2} alignItems="left">
+            <Typography variant="h4">Animation Enabled</Typography>
+            <Switch
+              checked={animEnabled}
+              size="medium"
+              onChange={(e) => {
+                const animEnabled = e.target.checked
+                if (animEnabled) {
+                  ledAnimOn(mqttClientRef.current)
+                } else {
+                  ledAnimOff(mqttClientRef.current)
+                }
+                setAnimEnabled(animEnabled)
+              }}
+            />
+          </Stack>
+        </Box>
+        <Stack my={2} direction="row" spacing={2} alignItems="center">
+          <TextField
+            label="Cur LED Duty (0-50)"
+            variant="filled"
+            value={ledDuty}
+            type="number"
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              const ledDuty = parseInt(event.target.value)
+              setLedDuty(ledDuty)
+              sendLedDuty(mqttClientRef.current, ledDuty)
+            }}
+          />
+          <Slider
+            value={ledDuty}
+            min={0}
+            max={50}
+            onChange={(_ev, val) => {
+              if (Array.isArray(val)) {
+                val = val[0]
+              }
+              setLedDuty(val)
+              sendLedDuty(mqttClientRef.current, val)
+            }}
+          />
+        </Stack>
+        <Stack my={2} direction="row" spacing={2} alignItems="center">
+          <TextField
+            label="Min sinwave duty (0 - 50)"
+            variant="filled"
+            value={minSinDuty}
+            type="number"
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              const minSinDuty = parseInt(event.target.value)
+              setMinSinDuty(minSinDuty)
+              sendMinSinDuty(mqttClientRef.current, minSinDuty)
+            }}
+          />
+          <Slider
+            value={minSinDuty}
+            min={0}
+            max={50}
+            onChange={(_ev, val) => {
+              if (Array.isArray(val)) {
+                val = val[0]
+              }
+              setMinSinDuty(val)
+              sendMinSinDuty(mqttClientRef.current, val)
+            }}
+          />
+        </Stack>
+        <Stack my={2} direction="row" spacing={2} alignItems="center">
+          <TextField
+            label="Max sinwave Duty (0-50)"
+            variant="filled"
+            value={maxSinDuty}
+            type="number"
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              const maxSinDuty = parseInt(event.target.value)
+              setMaxSinDuty(maxSinDuty)
+              sendMaxSinDuty(mqttClientRef.current, maxSinDuty)
+            }}
+          />
+          <Slider
+            value={maxSinDuty}
+            min={0}
+            max={50}
+            onChange={(_ev, val) => {
+              if (Array.isArray(val)) {
+                val = val[0]
+              }
+              setMaxSinDuty(val)
+              sendMaxSinDuty(mqttClientRef.current, val)
+            }}
+          />
+        </Stack>
+        <Stack my={2} direction="row" spacing={2} alignItems="center">
+          <TextField
+            label="Cur Anim Time (ms) (1000 - 20,000)"
+            variant="filled"
+            value={animTime}
+            type="number"
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              const animTime = parseInt(event.target.value)
+              setAnimTime(animTime)
+              sendAnimTime(mqttClientRef.current, animTime)
+            }}
+          />
+          <Slider
+            value={animTime}
+            min={1000}
+            max={20000}
+            onChange={(_ev, val) => {
+              if (Array.isArray(val)) {
+                val = val[0]
+              }
+              setAnimTime(val)
+              sendAnimTime(mqttClientRef.current, val)
+            }}
+          />
+        </Stack>
+      </Box>
+    </Container>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,285 +1,38 @@
-import { useState, useRef } from 'react'
-import type { MqttClient } from 'mqtt'
-import useMqtt from '../lib/useMqtt'
-import { handlerPayload } from '../lib/useMqtt'
+import StringLightsController from '../components/stringLightsController'
+import { Box, Container } from '@mui/material'
 
-import {
-  Box,
-  Button,
-  TextField,
-  Stack,
-  Container,
-  // Typography,
-  Slider,
-} from '@mui/material'
+const stringLightsTopicPrefix = '/LIGHTING'
+const stringLightsData = [
+  {
+    id: 'LIVING_ROOM',
+    name: 'Living Room',
+  },
+  {
+    id: 'MUSIC_ROOM',
+    name: 'Music Room',
+  },
+  {
+    id: 'DESK_ROOM',
+    name: 'Desk Room',
+  },
+]
+
+const renderLightControls = () => {
+  let stringLightsControllers = []
+  for (const stringLightData of stringLightsData) {
+    stringLightsControllers.push(
+      <Box my={6} key={stringLightData.id}>
+        <StringLightsController
+          topicPrefix={`${stringLightsTopicPrefix}/${stringLightData.id}`}
+          name={stringLightData.name}
+        />
+      </Box>
+    )
+  }
+
+  return stringLightsControllers
+}
 
 export default function Home() {
-  const [ledDuty, setLedDuty] = useState(0)
-  const [minSinDuty, setMinSinDuty] = useState(0)
-  const [maxSinDuty, setMaxSinDuty] = useState(50)
-  const [animTime, setAnimTime] = useState(4000)
-
-  const incommingMessageHandlers = useRef([
-    {
-      topic: '/sayHelloToUI',
-      handler: ({ payload }: handlerPayload) => {
-        console.log(payload)
-      },
-    },
-    // {
-    //   topic: '/temp',
-    //   handler: ({ payload }: handlerPayload) => {
-    //     setTemp(payload)
-    //   },
-    // },
-    // {
-    //   topic: '/altitude',
-    //   handler: ({ payload }: handlerPayload) => {
-    //     setAltitude(payload)
-    //   },
-    // },
-    // {
-    //   topic: '/tapDetected',
-    //   handler: ({ payload }: handlerPayload) => {
-    //     setNumTaps(payload)
-    //   },
-    // },
-  ])
-
-  const mqttClientRef = useRef<MqttClient | null>(null)
-  const setMqttClient = (client: MqttClient) => {
-    mqttClientRef.current = client
-  }
-  useMqtt({
-    uri: process.env.NEXT_PUBLIC_MQTT_URI,
-    options: {
-      // username: process.env.NEXT_PUBLIC_MQTT_USERNAME,
-      // password: process.env.NEXT_PUBLIC_MQTT_PASSWORD,
-      clientId: process.env.NEXT_PUBLIC_MQTT_CLIENTID,
-    },
-    topicHandlers: incommingMessageHandlers.current,
-    onConnectedHandler: (client) => setMqttClient(client),
-  })
-
-  const publishMessages = (client: any) => {
-    if (!client) {
-      console.log('(publishMessages) Cannot publish, mqttClient: ', client)
-      return
-    }
-
-    client.publish('topic1', '1st message from component')
-  }
-
-  const mainLedOn = (client: any) => {
-    if (!client) {
-      console.log('(ledEnable) Cannot publish, mqttClient: ', client)
-      return
-    }
-
-    client.publish('/ledEnable', 'true')
-  }
-
-  const mainLedOff = (client: any) => {
-    if (!client) {
-      console.log('(ledDisable) Cannot publish, mqttClient: ', client)
-      return
-    }
-
-    client.publish('/ledDisable', 'true')
-  }
-
-  const ledAnimOn = (client: any) => {
-    if (!client) {
-      console.log('(ledAnimEnable) Cannot publish, mqttClient: ', client)
-      return
-    }
-
-    client.publish('/ledAnimEnable', 'true')
-  }
-
-  const ledAnimOff = (client: any) => {
-    if (!client) {
-      console.log('(ledAnimOff) Cannot publish, mqttClient: ', client)
-      return
-    }
-
-    client.publish('/ledAnimDisable', 'true')
-  }
-
-  const sendLedDuty = (client: any) => {
-    if (!client) {
-      console.log('(setLedDuty) Cannot publish, mqttClient: ', client)
-      return
-    }
-
-    client.publish('/setLedDuty', ledDuty.toString(10))
-  }
-
-  const sendMinSinDuty = (client: any) => {
-    if (!client) {
-      console.log('(setMinSinDuty) Cannot publish, mqttClient: ', client)
-      return
-    }
-
-    client.publish('/setMinSinDuty', minSinDuty.toString(10))
-  }
-
-  const sendMaxSinDuty = (client: any) => {
-    if (!client) {
-      console.log('(setMaxSinDuty) Cannot publish, mqttClient: ', client)
-      return
-    }
-
-    client.publish('/setMaxSinDuty', maxSinDuty.toString(10))
-  }
-
-  const sendAnimTime = (client: any) => {
-    if (!client) {
-      console.log('(setAnimTime) Cannot publish, mqttClient: ', client)
-      return
-    }
-
-    client.publish('/setAnimTime', animTime.toString(10))
-  }
-
-  return (
-    <Container>
-      <Box my={6}>
-        <Box my={4}>
-          <Stack my={2} direction="row" spacing={2} alignItems="center">
-            <Button
-              variant="contained"
-              onClick={() => publishMessages(mqttClientRef.current)}
-            >
-              Say Hello
-            </Button>
-          </Stack>
-        </Box>
-        <Box my={4}>
-          <Stack my={2} direction="row" spacing={2} alignItems="center">
-            <Button
-              variant="contained"
-              onClick={() => mainLedOn(mqttClientRef.current)}
-            >
-              LEDs On
-            </Button>
-            <Button
-              variant="contained"
-              onClick={() => mainLedOff(mqttClientRef.current)}
-            >
-              LEDs Off
-            </Button>
-            <Button
-              variant="contained"
-              onClick={() => ledAnimOn(mqttClientRef.current)}
-            >
-              LEDs Anim On
-            </Button>
-            <Button
-              variant="contained"
-              onClick={() => ledAnimOff(mqttClientRef.current)}
-            >
-              LEDs Anim Off
-            </Button>
-          </Stack>
-        </Box>
-        <Stack my={2} direction="row" spacing={2} alignItems="center">
-          <TextField
-            label="Cur LED Duty (0-50)"
-            variant="filled"
-            value={ledDuty}
-            type="number"
-            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-              setLedDuty(parseInt(event.target.value))
-              sendLedDuty(mqttClientRef.current)
-            }}
-          />
-          <Slider
-            value={ledDuty}
-            min={0}
-            max={50}
-            onChange={(_ev, val) => {
-              if (Array.isArray(val)) {
-                val = val[0]
-              }
-              setLedDuty(val)
-              sendLedDuty(mqttClientRef.current)
-            }}
-          />
-        </Stack>
-        <Stack my={2} direction="row" spacing={2} alignItems="center">
-          <TextField
-            label="Min sinwave duty (0 - 50)"
-            variant="filled"
-            value={minSinDuty}
-            type="number"
-            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-              setMinSinDuty(parseInt(event.target.value))
-              sendMinSinDuty(mqttClientRef.current)
-            }}
-          />
-          <Slider
-            value={minSinDuty}
-            min={0}
-            max={50}
-            onChange={(_ev, val) => {
-              if (Array.isArray(val)) {
-                val = val[0]
-              }
-              setMinSinDuty(val)
-              sendMinSinDuty(mqttClientRef.current)
-            }}
-          />
-        </Stack>
-        <Stack my={2} direction="row" spacing={2} alignItems="center">
-          <TextField
-            label="Max sinwave Duty (0-50)"
-            variant="filled"
-            value={maxSinDuty}
-            type="number"
-            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-              setMaxSinDuty(parseInt(event.target.value))
-              sendMaxSinDuty(mqttClientRef.current)
-            }}
-          />
-          <Slider
-            value={maxSinDuty}
-            min={0}
-            max={50}
-            onChange={(_ev, val) => {
-              if (Array.isArray(val)) {
-                val = val[0]
-              }
-              setMaxSinDuty(val)
-              sendMaxSinDuty(mqttClientRef.current)
-            }}
-          />
-        </Stack>
-        <Stack my={2} direction="row" spacing={2} alignItems="center">
-          <TextField
-            label="Cur Anim Time (ms) (1000 - 20,000)"
-            variant="filled"
-            value={animTime}
-            type="number"
-            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-              setAnimTime(parseInt(event.target.value))
-              sendAnimTime(mqttClientRef.current)
-            }}
-          />
-          <Slider
-            value={animTime}
-            min={1000}
-            max={20000}
-            onChange={(_ev, val) => {
-              if (Array.isArray(val)) {
-                val = val[0]
-              }
-              setAnimTime(val)
-              sendAnimTime(mqttClientRef.current)
-            }}
-          />
-        </Stack>
-      </Box>
-    </Container>
-  )
+  return <Container>{renderLightControls()}</Container>
 }
